ef01da2162ccb2b4d0b5f9f8c5d0cf38342410e3
iosip <iosipoi@gmail.com>
2022-10-22 11:13:53 +0300

    Add project files.

---
+++ PollApi/Survey.cs
@@ -0,0 +1,11 @@
+﻿namespace PollApi
+{
+    public class Survey
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
---
+++ pollmdreact/src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    static displayName = App.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { surveys: [], loading: true };
+    }
+
+    componentDidMount() {
+        this.populateSurveyData();
+    }
+
+    static renderSurveyTable(surveys) {
+        return (
+            <table className='table table-striped' aria-labelledby="tabelLabel">
+                <tbody>
+                    {surveys.map(survey =>
+                        <tr key={survey.id}>
+                            <td>{survey.name}</td>
+                            <td>{survey.description}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading... </em></p>
+            : App.renderSurveyTable(this.state.surveys);
+
+        return (
+            <div>
+                <h1 id="tabelLabel" >Surveys</h1>
+                <p>This component demonstrates fetching data from the server.</p>
+                {contents}
+            </div>
+        );
+    }
+
+    async populateSurveyData() {
+        const response = await fetch('survey');
+        const data = await response.json();
+        this.setState({ surveys: data, loading: false });
+    }
+}
